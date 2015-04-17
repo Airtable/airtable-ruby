f4cb0f9f3d986057f2349c850638bd10631b7f79
Nathan Esquenazi <nesquena@gmail.com>
2015-04-16 23:40:37 -0700

    Adds support for querying single set of records and sorting

--- lib/airtable.rb
+++ lib/airtable.rb
@@ -1,7 +1,9 @@
 require 'httparty'
+require 'delegate'
 require "airtable/version"
 require 'airtable/resource'
 require 'airtable/record'
+require 'airtable/record_set'
 require 'airtable/table'
 require 'airtable/client'
 
--- lib/airtable/table.rb
+++ lib/airtable/table.rb
@@ -1,23 +1,36 @@
 module Airtable
 
   class Table < Resource
+    # Maximum results per request
+    LIMIT_MAX = 100
 
-    # Fetch all records iterating through offsets
-    def all
+    # Fetch all records iterating through offsets until retrieving the entire collection
+    # all(:sort => ["Name", :desc])
+    def all(options={})
       offset = nil
       results = []
       begin
-        records = self.class.get(worksheet_url, query: { 'limit' => 100, 'offset' => offset }).parsed_response
-        results += records["records"].map { |r| Record.new(r["fields"].merge("id" => r["id"])) }
-        offset = records["offset"]
-      end until offset.nil?
+        options.merge!(:limit => LIMIT_MAX, :offset => offset)
+        response = records(options)
+        results += response.records
+        offset = response.offset
+      end until offset.nil? || offset.empty? || results.empty?
       results
+    end
+
+    # Fetch records from the sheet given the list options
+    # Options: limit = 100, offset = "as345g", sort = ["Name", "asc"]
+    # records(:sort => ["Name", :desc], :limit => 50, :offset => "as345g")
+    def records(options={})
+      options["sortField"], options["sortDirection"] = options.delete(:sort) if options[:sort]
+      results = self.class.get(worksheet_url, query: options).parsed_response
+      RecordSet.new(results)
     end
 
     # Returns record based given row id
     def find(id)
       result = self.class.get(worksheet_url + "/" + id).parsed_response
-      Record.new(result["fields"].merge("id" => result["id"])) if result.present? && result["id"].present?
+      Record.new(result["fields"].merge("id" => result["id"])) if result.present? && result["id"]
     end
 
     # Creates a record by posting to airtable
