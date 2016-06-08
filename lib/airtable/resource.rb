912451f3d535b313079da7d93209a9689d56809d
Alexander Sorokin <syrnick@gmail.com>
2016-06-08 07:36:12 -0700

    Add support for sorting records by multiple fields

--- lib/airtable/resource.rb
+++ lib/airtable/resource.rb
@@ -1,11 +1,17 @@
+require 'airtable/httparty_patched_hash_conversions'
+
 module Airtable
   # Base class for authorized resources sending network requests
   class Resource
     include HTTParty
-    base_uri 'https://api.airtable.com/v0/'
+    base_uri (ENV['AIRTABLE_ENDPOINT_URL'] || 'https://api.airtable.com/') + 'v0/'
     # debug_output $stdout
 
     attr_reader :api_key, :app_token, :worksheet_name
+
+    query_string_normalizer(proc do |query|
+      HTTParty::PatchedHashConversions.to_params(query)
+    end)
 
     def initialize(api_key, app_token, worksheet_name)
       @api_key = api_key
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,11 +4,10 @@ require 'minitest/pride'
 require 'minitest/autorun'
 
 def stub_airtable_response!(url, response, method=:get, status=200)
-
   stub_request(method, url)
     .to_return(
       body: response.to_json,
       status: status,
-      headers: { 'Content-Type' =>  "application/json"}
+      headers: { 'Content-Type' => 'application/json' }
     )
 end
